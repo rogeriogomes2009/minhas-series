b2c88af8157c54e7d1449bf31f8bcc71084bc47e
rogeriogomes2009 <rogerio.gomes2009@gmail.com>
2022-01-31 15:46:07 -0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+const port = process.env.PORT || 3000
+const mongo = process.env.MONGO || 'mongodb://localhost/minhas-series'
+
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+
+// process request body
+app.use(bodyParser.urlencoded({ extended: true }))
+
+//assets
+app.use(express.static('public'))
+
+// view engine - EJS
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => res.render('index'))
+
+mongoose
+.connect(mongo, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => {
+  app.listen(port, () => {
+    console.log('Listening on: '+port)
+  })
+})
+.catch( e => {
+  console.log(e)
+})
+
