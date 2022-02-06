eda9d78326d4f4f69d3b367d6131dd8c71081df1
rogeriogomes2009 <rogerio.gomes2009@gmail.com>
2022-02-05 22:44:28 -0300

    Update index

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ const series = require('./routes/series')
 
 const port = process.env.PORT || 3000
 const mongo = process.env.MONGO || 'mongodb://localhost/minhas-series'
-
+/*
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-
+*/
 // process request body
 app.use(bodyParser.urlencoded({ extended: true }))
 
 //assets
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 // view engine - EJS
 app.set('views', path.join(__dirname, 'views'))
@@ -25,15 +25,16 @@ app.set('view engine', 'ejs')
 app.use('/', pages)
 app.use('/series', series)
 
-
+app.listen(port, () => {
+  console.log('Listening on: '+port)
+/*
 mongoose
 .connect(mongo, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => {
-  app.listen(port, () => {
-    console.log('Listening on: '+port)
+  
   })
 })
 .catch( e => {
-  console.log(e)
+  console.log(e)*/
 })
 
